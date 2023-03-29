cee1da78481f120b81447f274d2ccb22acc7285e
unknown <baselyasser540@gmail.com>
2023-03-29 22:34:04 +0200

    Bassel_V2

--- Terminal/terminal.c
+++ Terminal/terminal.c
@@ -2,6 +2,7 @@
  *                                                      Includes
 **************************************************************************************************************************/
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "../Common/STD_types.h"
 #include "../Card/card.h"
@@ -14,14 +15,62 @@
 EN_terminalError_t getTransactionDate( ST_terminalData_t *termData )
 {
     EN_terminalError_t errorStatus;
+    uint8_t transDate[50];
+    int32_t x, flag = 0;
 
-    if(termData != NULL)
+    printf("Please Enter Transaction Date: ");
+    scanf("%s", transDate);
+
+    char* token = strtok(transDate, "/");
+    while(token != NULL)
     {
-        gets( termData->transactionDate );
+        x = atoi(token);
+        if ( flag == 0  )
+        {
+            if ( x > 0 && x < 31 )
+            {
+                flag = 1;
+            }
+            else 
+            {
+                break;
+            }
+        }
+        else if (flag == 1)
+        {
+            if ( x >= 1 && x <= 12 )
+            {
+                flag = 2;
+            }
+            else 
+            {
+                break;
+            }
+        }
+        else if ( flag == 2 )
+        {
+            if ( x > 2000 && x < 2100 )
+            {
+                flag = 3;
+            }
+            else 
+            {
+                break;
+            }
+        }
+
+        token = strtok(NULL, "/");
+    }
+
+    
+    if(termData == NULL || strlen(transDate) < 10 || flag != 3)
+    {
+        errorStatus = WRONG_DATE;
     }
     else 
     {
-        errorStatus = INVALID_CARD;
+        errorStatus = TERMINAL_OK;
+        strcpy( termData->transactionDate, transDate );
     }
 
 
@@ -52,12 +101,23 @@ EN_terminalError_t setMaxAmount(ST_terminalData_t *termData , float maxAmount)
     EN_terminalError_t errorStatus;
     if (termData != NULL)
     {
-        errorStatus = TERMINAL_OK;
-        termData->maxTransAmount = maxAmount;
+        if ( maxAmount > 0 )
+        {
+            errorStatus = TERMINAL_OK;
+            termData->maxTransAmount = maxAmount;
+        }
+        else if ( maxAmount <= 0 )
+        {
+            errorStatus = INVALID_MAX_AMOUNT;
+        }
+        else 
+        {
+            /*do nothing*/
+        }
     }
     else 
     {
-        errorStatus = INVALID_CARD;
+        /* do nothing */
     }
 
     return errorStatus;
