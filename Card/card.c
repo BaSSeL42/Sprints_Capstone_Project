2d31f0cf96b80f54a9e55678e07f54daf5675c6c
AalaaIbrahim <aalaa.h.ibrahim@gmail.com>
2023-03-30 05:04:21 +0200

    added (getCardHolderName) and related macros

--- Card/card.c
+++ Card/card.c
@@ -12,7 +12,33 @@
 **************************************************************************************************************************/
 EN_cardError_t getCardHolderName(ST_cardData_t* cardData)
 {
-	/* ToDo */
+	uint8_t size, Local_Iterator=0;
+
+	printf("Card Holder Name: ");
+	scanf("%[^\n]%*c", cardData->cardHolderName);
+	printf("%s\n", cardData->cardHolderName);
+	
+	/* Check whether the name is within limits */
+	size = strlen(cardData->cardHolderName);
+	if (size < 20 || size > 24)
+	{
+		return WRONG_NAME;
+	}
+
+	/* Check that all characters are alphabetic */
+	while (cardData->cardHolderName[Local_Iterator] != '\0')
+	{
+		if ((cardData->cardHolderName[Local_Iterator] < A_ASCII_INDEX || cardData->cardHolderName[Local_Iterator] > Z_ASCII_INDEX)
+			&& (cardData->cardHolderName[Local_Iterator] < a_ASCII_INDEX || cardData->cardHolderName[Local_Iterator] > z_ASCII_INDEX)
+			&& cardData->cardHolderName[Local_Iterator] != SPACE_ASCII_INDEX)
+		{
+			return WRONG_NAME;
+		}
+
+		Local_Iterator++;
+	}
+
+	return CARD_OK;
 }
 
 
